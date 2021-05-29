17bde3e105ac8739dc53fa5720e2adf96096b32f
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-05-29 18:34:40 -0400

    Initial Commit

---
+++ SortingMethods/SortingMethodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingMethodsApp.swift
+//  SortingMethods
+//
+//  Created by Hung-Chun Tsai on 2021-05-29.
+//
+
+import SwiftUI
+
+@main
+struct SortingMethodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
